6c4500bcf8ab2b9aa63684119c4d0bfa96b06bd5
ibrahimothman <ibrahimothmanre@gmail.com>
2021-09-14 17:48:14 +0400

    update README.md

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 const firebaseApp = initializeApp({
-    apiKey: "AIzaSyCypTsdg4-mt-xU5MH1ICfvSZbVgEo4z_U",
-    authDomain: "tasawaq-a225f.firebaseapp.com",
-    databaseURL: "https://tasawaq-a225f.firebaseio.com",
-    projectId: "tasawaq-a225f",
-    storageBucket: "tasawaq-a225f.appspot.com",
-    messagingSenderId: "945072771910",
-    appId: "1:945072771910:web:2dd8687b05e01adf7e0b47"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 });
 
 
